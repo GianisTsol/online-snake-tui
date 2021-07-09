1d69fc5629d3caf23d90c2fb941622b34847e9e7
Kafros <35958401+GianisTsol@users.noreply.github.com>
2021-07-09 23:04:47 +0300

    Update src/__init__.py
    Co-authored-by: Artemis <hollyshort21@gmail.com>

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,6 @@
-"""Module Innit."""
+"""Beatific Bulldogs' code jam submission.
+
+TODO: Replace with actual description of the program.
+"""
+
+# This file exists to denote to Python that the folder is a package.
