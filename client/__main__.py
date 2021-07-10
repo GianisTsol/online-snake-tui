01f6233327ecb22b981f3466c1235dfee1794af8
x7a <>
2021-07-10 21:55:01 +0200

    Add basic game loop and window class

--- client/__main__.py
+++ client/__main__.py
@@ -1,4 +1,19 @@
 """Entrypoint for the TUI client."""
 
+import time
+
+from .models import Window
+
+FPS = 60
+
 if __name__ == '__main__':
-    pass  # TODO: Run the app.
+    window = Window()
+
+    current_time = last_frame_time = time.time()
+    while True:  # Game loop
+        # Calculations needed for maintaining stable FPS
+        sleep_time = 1. / FPS - (current_time - last_frame_time)
+        if sleep_time > 0:
+            time.sleep(sleep_time)
+
+        window.redraw()
---
+++ client/models.py
@@ -0,0 +1,34 @@
+"""Models responsible for client side game run."""
+import os
+
+from blessed import Terminal
+
+
+class Window:
+    """Class for rendering game on the screen."""
+
+    CHARS = ['┌', '─', '┐', '│', '└', '┘']
+
+    def __init__(self):
+        """TODO: Docstring."""
+        self.term = Terminal()
+        self.width, self.height = os.get_terminal_size()
+        self.BORDER_COLOR = self.term.bright_green
+
+    def redraw(self):
+        """TODO: Docstring."""
+        print(self.term.home + self.term.clear, end='')  # Clear the screen
+
+        # Print the border header
+        print(self.BORDER_COLOR + self.CHARS[0] + self.CHARS[1]
+              * (self.width - 2) + self.CHARS[2] + self.term.normal)
+
+        # Print border sides
+        for i in range(self.height - 3):
+            print(self.BORDER_COLOR + self.CHARS[3] + self.term.normal + ' '
+                  * (self.width - 2) + self.BORDER_COLOR
+                  + self.CHARS[3] + self.term.normal)
+
+        # Print the border footer
+        print(self.BORDER_COLOR + self.CHARS[4] + self.CHARS[1]
+              * (self.width - 2) + self.CHARS[5] + self.term.normal)
