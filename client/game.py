671237801d98898b1ea257fefbf9330c8486d8c3
Artemis <hollyshort21@gmail.com>
2021-07-12 16:33:20 +0100

    Make the snake prettier using lots of box drawing characters (#8)
    * Make snakes prettier using lots of box chars.
    
    Also, add more death messages.
    
    * Change isort import wrapping style.
    
    authored-by: Artemis21 <artemisdev21@gmail.com>

--- client/game.py
+++ client/game.py
@@ -9,6 +9,45 @@ from .level import Window
 BLOCK_CHAR = '█'
 FPS = 15
 
+# The numbers are lower score boundaries for each verdicts.
+DEATH_VERDICTS = [
+    (0, 'Pathetic.'),
+    (1, 'Try harder next time?'),
+    (5, 'Almost good.'),
+    (10, 'Not bad...'),
+    (15, 'Decent.'),
+    (20, 'Well done.'),
+    (25, 'Nice!'),
+    (30, 'Sweet!'),
+    (45, 'Amazing!!'),
+    (50, 'Wow!!! What a game!'),
+    (100, 'The bestest of the best!!!'),
+]
+
+# N[orth]/E[ast]/S[outh]/[W]est indicates direction of segment to join on to.
+SNAKE_HEAD_CHARS = {
+    'n': '╹',
+    'e': '╺',
+    's': '╻',
+    'w': '╸',
+}
+
+SNAKE_BODY_CHARS = {
+    'ew': '═',
+    'ns': '║',
+    'es': '╔',
+    'sw': '╗',
+    'ne': '╚',
+    'nw': '╝',
+}
+
+SNAKE_TAIL_CHARS = {
+    'n': '╿',
+    'e': '╼',
+    's': '╽',
+    'w': '╾',
+}
+
 
 class GameController:
     """The game mainloop."""
@@ -24,7 +63,6 @@ class GameController:
         self.direction = direction
         self.segments = []
         self.score = 0
-        self.lose_msg = f"You died with: {self.score} score. pathetic."
 
         # Create the initial snake segments.
         for _ in range(logic.STARTING_SNAKE_SEGMENTS):
@@ -32,27 +70,65 @@ class GameController:
         with self.term.hidden_cursor():
             self.run_game_loop()
 
+    def direction_from_segment(
+            self, from_segment_index: int, to_segment_index: int) -> str:
+        """Find the direction from one segment to another.
+
+        Assumes these segments are connected.
+        """
+        s_from = self.segments[from_segment_index]
+        s_to = self.segments[to_segment_index]
+        if s_from.x == s_to.x:
+            return 'n' if s_to.y < s_from.y else 's'
+        else:
+            return 'e' if s_to.x > s_from.x else 'w'
+
     def draw(self):
         """Draw the snake on the window."""
-        for i in self.segments:
+        snake_chars = []
+        snake_chars.append(SNAKE_HEAD_CHARS[self.direction_from_segment(0, 1)])
+        for body_index in range(1, len(self.segments) - 1):
+            before_dir = self.direction_from_segment(
+                body_index, body_index - 1
+            )
+            after_dir = self.direction_from_segment(body_index, body_index + 1)
+            if before_dir + after_dir in SNAKE_BODY_CHARS:
+                snake_chars.append(SNAKE_BODY_CHARS[before_dir + after_dir])
+            else:
+                snake_chars.append(SNAKE_BODY_CHARS[after_dir + before_dir])
+        snake_chars.append(
+            SNAKE_TAIL_CHARS[self.direction_from_segment(-1, -2)]
+        )
+        for segment, char in zip(self.segments, snake_chars):
             print(
                 self.term.home
-                + self.term.move_xy(i.x, i.y)
+                + self.term.move_xy(segment.x, segment.y)
                 + self.window.SNAKE_COLOR
-                + BLOCK_CHAR
+                + char
                 + self.term.normal
                 + self.term.home
             )
 
+    def get_death_message(self) -> str:
+        """Get the message to show when the player dies."""
+        for score, possible_verdict in DEATH_VERDICTS:
+            if score <= self.score:
+                verdict = possible_verdict
+        return f'You died with: {self.score} score. {verdict}'
+
     def show_death_screen(self):
-        """Show when player loses."""
-        x = self.term.width // 2
+        """Show when player dies."""
+        message = self.get_death_message()
+        # Calculate center.
+        x = self.term.width // 2 - len(message) // 2
         y = self.term.height // 2
-        # calculate center
-        x = x - len(self.lose_msg) // 2
         print(end=self.term.home + self.term.clear)
-        color = self.term.red
-        print(self.term.move_xy(x, y) + color + self.lose_msg + self.term.normal)
+        print(
+            self.term.move_xy(x, y)
+            + self.term.red
+            + message
+            + self.term.normal
+        )
 
     def run_game_loop(self):
         """Run the game update loop."""
