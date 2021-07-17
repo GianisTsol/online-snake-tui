88132a6b425929e4c7ac5b914bd60a687e689ad9
GianisTsol <giannisetsolakis@gmail.com>
2021-07-17 03:11:53 +0300

    MAde wall collision work.

--- common/logic.py
+++ common/logic.py
@@ -118,4 +118,6 @@ def has_collided_with_wall(
 ) -> bool:
     """Return True if the snake has collided with a wall."""
     head = segments[0]
-    return head.x < 0 or head.x > width - 1 or head.y < 0 or head.y > height - 1
+    return (
+        head.x <= 1 or head.x > width - 3 or head.y < 1 or head.y >= height - 2
+    )
